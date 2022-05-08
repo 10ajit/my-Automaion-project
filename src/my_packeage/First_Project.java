0a83ae8909be66fda47e4297ead17f1831e694b1
10ajit <10ajit@gmail.com>
2022-05-08 11:56:14 +0530

    my code

---
+++ src/my_packeage/First_Project.java
@@ -0,0 +1,23 @@
+package my_packeage;
+
+import java.util.ArrayList;
+
+public class First_Project {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>list =new ArrayList<Integer>();
+		
+	
+		//How to add Element in Arraylist
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(8);
+		list.add(5);
+
+		System.out.println(list);
+		
+		
+	}
+
+}
