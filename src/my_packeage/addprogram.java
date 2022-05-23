154ce1c6ef51bceda4c3439d05afbd1b6ce75ffe
10ajit <10ajit@gmail.com>
2022-05-23 07:15:03 +0530

    Signed-off-by: 10ajit <10ajit@gmail.com>

---
+++ src/my_packeage/addprogram.java
@@ -0,0 +1,48 @@
+package my_packeage;
+
+public class addprogram {
+
+	public static void main(String[] args) {
+int a[]= {20,30,300};
+int sum=0;
+for (int i = 0; i<=a.length-1; i++) {
+	sum=sum+a[i];
+}
+System.out.println(sum);	
+		
+			
+		
+		
+//String a= "Ajit"; 
+//String b= "kumar";
+//System.out.println(a+" "+b);
+
+		
+		
+		
+		
+//	int a=10;
+//	int b=20;
+//	int c=30;
+//	int d=a+b+c;
+//	
+//	//System.out.println(a+b+c);
+//	
+//	System.out.println("a+b+c"+"="+d);
+//	
+		
+		
+		
+		
+		
+		
+		
+		
+//		int a=5;
+//		int b=8;
+//		int res=a+b;
+//		System.out.println(res);
+
+	}
+
+}
